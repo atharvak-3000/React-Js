4f9e7ee74014a9b10d569d2b52e89dd3b9f1cd27
Atharva Kale <atharvakale20536@gmail.com>
2025-11-21 15:45:33 +0530

    ...

---
+++ 05-css/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from './Components/button/Button'
+import Header from './Components/Header'
+
+const App = () => {
+  return (
+    <div className='parent'>
+      <Header/>
+      <Button/>
+    </div>
+  )
+}
+
+export default App
---
+++ 05-css/src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from './button/Button'
+import '../styles/header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <h1>Sheriyans</h1>
+        <button>Login</button>
+    </div>
+  )
+}
+
+export default Header
